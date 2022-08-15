8b399797de8e38595f51f207fdd44a5a3282dde2
KonradMiedla <konrad.miedla@gmail.com>
2022-08-15 21:34:25 +0200

    discover+footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,8 +1,25 @@
 import React from 'react'
 
+import { footerList1, footerList2, footerList3 } from '../utils/constants';
+
+const List = ({ items, mt }: {items: string[], mt: Boolean}) => (
+  <div className={`flex flex-wrap gap-2 ${mt && 'mt-5'}`}>
+    {items.map((item) => (
+      <p key={item} className='text-gray-400 text-sm hover:underline cursor-ponter'>
+        {item}
+      </p>
+    ))}
+  </div>
+)
+
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <div className='mt-6 hidden xl:block'>
+      <List items={footerList1} mt={false}></List>
+      <List items={footerList2} mt></List>
+      <List items={footerList3} mt></List>
+      <p className='text-gray-400 text-sm mt-5'>2022 TikTik</p>
+    </div>
   )
 }
 
