c440fd892f07237dd7e9a77de735d746f093cb80
KonradMiedla <konrad.miedla@gmail.com>
2022-09-16 23:55:16 +0200

    video details

--- pages/detail/[id].tsx
+++ pages/detail/[id].tsx
@@ -6,6 +6,7 @@ import { GoVerified } from 'react-icons/go';
 import { MdOutlineCancel } from 'react-icons/md';
 import { BsFillPlayFill } from 'react-icons/bs';
 import { HiVolumeUp, HiVolumeOff } from 'react-icons/hi';
+
 import axios from 'axios';
 import { BASE_URL } from '../../utils';
 import { Video } from '../../types';
@@ -16,10 +17,76 @@ interface IProps {
 
 const Detail = ({ postDetails }: IProps) => {
   const [post, setPost] = useState(postDetails);
+  const [isPlaying, setIsPlaying] = useState(false);
+  const [isVideoMuted, setIsVideoMuted] = useState(false);
+  const videoRef = useRef<HTMLVideoElement>(null);
+  const router = useRouter();
 
-  const videoRef = useRef(null);
+  const onVideoClick = () => {
+    if(isPlaying) {
+      videoRef.current?.pause();
+      setIsPlaying(false);
+    } else {
+      videoRef.current?.play();
+      setIsPlaying(true);
+    }
+  }
+
+  useEffect(() => {
+    if(post && videoRef?.current) {
+        videoRef.current.muted = isVideoMuted;
+    }
+  }, [post, isVideoMuted])
+
+
+  if(!post) return null;
+
   return (
-    <div>Detail</div>
+    <div className='flex w-full absolute left-0 top-0 bg-white flex-wrap lg:flex-nowrap'>
+      <div className='relative flex-2 w-[1000px] lg:w-p/12 flex justify-center items-center bg-blurred-img bg-no-repeat bg-cover bg-center'>
+        <div className='absolute top-6 left-2 lg:left-6 flex gap-6 z-50'>
+          <p className='cursor-pointer' onClick={() => router.back()}>
+            <MdOutlineCancel className='text-white text-[35px]'></MdOutlineCancel>
+          </p>
+        </div>
+        <div className='relative'>
+          <div className='lg:h-[100vh] h-[60vh]'>
+            <video
+              ref={videoRef}
+              loop
+              onClick={onVideoClick}
+              src={post.video.asset.url}
+              className="h-full cursor-pointer"
+            ></video>
+          </div>
+
+          <div className='absolute top-[45%] left-[45%] cursor-pointer'>
+            {!isPlaying && (
+              <button onClick={onVideoClick}>
+                <BsFillPlayFill className='text-white text-6xl lg:text-8xl'></BsFillPlayFill>
+              </button>
+            )}
+          </div>
+        </div>
+
+        <div className='absolute bottom-5 lg:bottom-10 right-5 lg:right-10 cursor-pointer'>
+          {isVideoMuted ? (
+            <button onClick={() => setIsVideoMuted(false)}>
+              <HiVolumeOff className='text-white text-2xl lg:text-4xl'>
+
+              </HiVolumeOff>
+            </button>
+          ) : (
+            <button onClick={() => setIsVideoMuted(true)}>
+              <HiVolumeUp className='text-white text-2xl lg:text-4xl'>
+                    
+              </HiVolumeUp>
+            </button>
+          )}
+        </div>
+
+      </div>
+    </div>
   )
 }
 
@@ -31,7 +98,7 @@ export const getServerSideProps = async ({
   const { data } = await axios.get(`${BASE_URL}/api/post/${id}`)
 
   return {
-    props: { postDetails: data };
+    props: { postDetails: data },
   }
 }
 
