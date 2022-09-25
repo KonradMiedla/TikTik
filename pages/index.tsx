7d95ab1f4dd38ef2bd88366ff9b2815a9dd703ef
KonradMiedla <konrad.miedla@gmail.com>
2022-09-26 00:19:52 +0200

    popular topic fix

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,6 +5,7 @@ import { Video } from '../types';
 import VideoCard from '../components/VideoCard';
 import NoResults from '../components/NoResults';
 import { BASE_URL } from '../utils';
+import { responseSymbol } from 'next/dist/server/web/spec-compliant/fetch-event';
 
 interface IProps {
   videos: Video[];
@@ -24,12 +25,22 @@ const Home = ({ videos }: IProps) => {
   )
 }
 
-export const getServerSideProps = async () => {
-  const { data } = await axios.get(`${BASE_URL}/api/post`);
+export const getServerSideProps = async ({
+  query: { topic }
+}: {
+  query: { topic: string }
+}) => {
+  let response = null;
+
+  if(topic) {
+    response = await axios.get(`${BASE_URL}/api/discover/${topic}`)
+  } else {
+    response = await axios.get(`${BASE_URL}/api/post`);
+  }
 
   return {
     props: {
-      videos: data
+      videos: response.data
     }
   }
 }
